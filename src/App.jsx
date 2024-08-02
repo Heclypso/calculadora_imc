a2f8fa41f140d390e8cd831208490f8754e05099
Gabriel Munhak <heclypso@gmail.com>
2024-08-02 11:52:39 -0300

    Personalizado site e criado mensagem de erro

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,20 @@
 import { useState } from "react";
 
 import Perfil from "./components/Perfil";
-import Formulario from "./components/Formulario"
 import ReposList from "./components/ReposList";
 
 function App() {
- const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(true);
  const [nomeUsuario, setNomeUsuario ] = useState('');
 
  return (
   <>
-  <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)} />
-
+  <input type="text" placeholder="Digite um nome de usuario do GitHub" onBlur={(e) => setNomeUsuario(e.target.value)} />
   {nomeUsuario.length > 4 && (
     <>
-      <Perfil nomeUsuario={nomeUsuario}/>
-      <ReposList nomeUsuario={nomeUsuario}/>
+    <Perfil nomeUsuario={nomeUsuario}/>
+    <ReposList nomeUsuario={nomeUsuario}/>
     </>
-  )
-
-  /* {formularioEstaVisivel && (
-    <Formulario />
-  )}
-
-  <button onClick={() => setFormularioEstaVisivel(!formularioEstaVisivel)} type="button">toggle form</button> */}
+  )} 
   </>
 
  )
